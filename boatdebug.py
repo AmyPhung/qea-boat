e032d9c97454bdf7c65a0f92b61d0e6b1e24a0ea
Kawin Nikomborirak <concavemail@gmail.com>
2018-02-18 17:43:13 +0000

    more pictures

--- boatdebug.py
+++ boatdebug.py
@@ -4,45 +4,16 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-# mvp
-# D = .1
-# W = .524
-# L = .
-# m = 1.4
-# ds = .003
-# cg = np.array([0, 0, .055])
-
-# real
-# D = .10
-# W = .1524
-# L = .58
-# m = .8997
-# ds = .003
-# cg = np.array([0, 0, .035])
-
-# narrow real
-# D = .075
-# W = .15
-# L = .7112
-# m = 1
-# ds = .002
-# cg = np.array([0, 0, .0170])
-
-# D = 1
-# W = 1
-# L = 1
-# m = 500
-# ds = 0.015
-# cg = np.array([0,0,.5])
-
 D = .075
-W = .132
+W = .15
 L = .5842
+# L =.4342
+# m = 1.036
 m = 1
-ds = .005
-# cg = np.array([0, 0, .0199])
-# cg = np.array([0, 0, 0.026])
-cg = np.array([0, 0, 0.0072])
+ds = .001
+cg = np.array([0, 0, .0135])
+# 120: cg = np.array([0, 0, .0242])
+# 140: cg = np.array([0, 0, 00000])
 
 X, Y, Z = np.meshgrid(np.arange(-L / 2, L / 2, ds),
                       np.arange(-W / 2, W / 2, ds),
@@ -50,26 +21,11 @@ X, Y, Z = np.meshgrid(np.arange(-L / 2, L / 2, ds),
 
 
 def hull(x, y):
-    # return -(W**2 / 4 - y ** 2) ** .5 + W / 2
-    # return D * (4 * x ** 2 / L ** 2 + 4 * y ** 2 / W ** 2)
-    # return - ((W / 2 - 128 * W * x ** 8 / L ** 8) ** 2 - y ** 2) ** .5 + W / 2
-    # r = W / 2
-    # r = 0.075
-    # return 0.05 * np.tan(np.abs(22 * y) - .9) + .01
-    return .012 * np.tan(np.abs(39 * y) - 1.2) + .01
-    # return y
-    # return 75 * np.tan(np.abs(y) / .075 * 1.2 - .6) + .01
-    # return np.tan(y)
-    # return y
-
-    # cube
-    # return 0
+    return .012 * np.tan(np.abs(39 * y) - 1.5) + .01
 
 
 hullMat = Z > hull(X, Y)
-
 volume = np.sum(hullMat * ds ** 3)
-
 
 def tilt(theta):
     b = intercept(theta)
@@ -104,9 +60,6 @@ def cb(theta):
     '''Returns the center of bouyancy'''
     waterMat = tilt(theta)
     waterMasses = waterMat * ds ** 3 * 1000
-    # plt.imshow(waterMat[18,:,:])
-    # plt.imshow(hullMat[18,:,:])
-    # plt.show()
     waterMass = np.sum(waterMasses)
     return np.array([np.sum(X * waterMasses) / waterMass,
                      np.sum(Y * waterMasses) / waterMass,
@@ -115,30 +68,13 @@ def cb(theta):
 
 def rightingMoment(theta):
     '''Returns the righting moment'''
-    # cb = cb(theta)[1:]
-    # r = (cb - cm[1:])
-    # fb = np.array([0, 0, g * m])  # bouyant force
     rotation = np.reshape((np.array([1, 0, 0, 0, np.cos(
         theta), -np.sin(theta), 0, np.sin(theta), np.cos(theta)])), (3, 3))
     cbs = cb(theta)
     r = cbs - cg
-    # print(cb)
     fb = np.dot(rotation, np.array([0, 0, m * g]))
     return (np.cross(r, fb)[0])
-    # return [0,0,0]
 
-
-# cb(0)
-
-# def intercept(
-# plt.imshow(tilt(np.pi / 3)[:, 100, :])
-# plt.show()
-# plt.imshow(hullMat[:, 58, :])
-# ax = plt.gca()
-# ax.set_aspect('equal')
-# plt.imshow(tilt(0)[:, 356, :])
-# plt.show()
-# print(np.sum((hullMat)))
 
 # plt.rc('text', usetex=True)
 # plt.rc('font', family='serif')
@@ -151,7 +87,7 @@ def rightingMoment(theta):
 # plt.show()
 # ax.figure.savefig('curve.png')
 
-print(rightingMoment(140 * np.pi / 180))
+# print(rightingMoment(130 * np.pi / 180))
 
 # newX, newY = np.meshgrid(np.arange(-L / 2, L / 2, ds),
 #                       np.arange(-W / 2, W / 2, ds))
@@ -163,6 +99,9 @@ print(rightingMoment(140 * np.pi / 180))
 # ax = fig.add_subplot(111, projection='3d')
 # ax.plot_surface(newNewX, newNewY, hull(newNewX, newNewY), cmap = cm.coolwarm)
 
-# ax.figure.savefig('badlyscaledbutbiggerhull.png')
+# ax.figure.savefig('curve.png')
 
 # plt.show()
+waterMat = tilt(0)
+plt.imshow(waterMat[:,0,:])
+plt.show()
